ed1e98f7064fd1857f33ebf54b3fc4ffb125c4e0
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-10-10 20:48:20 +0400

    NextAuth.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { SessionProvider, signIn, signOut, useSession } from 'next-auth/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <header><Login /></header>
+      <hr/>
+      <main><Component {...pageProps} /></main>
+    </SessionProvider>
+  );
+}
+
+function Login() {
+  const { data: session, status } = useSession();
+  if ('loading' === status)
+    return <button>вЊ›</button>;
+  if (session)
+    return <>
+      {session?.user?.image && <img src={session?.user?.image || ''} width={32} height={32} alt="ava" />}
+      {session?.user?.name}
+      <button onClick={() => signOut()}>Sign out</button>
+    </>;
+  return <>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>;
 }
